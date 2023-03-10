839902479665d1357c3b75e1ca7c84575dcdc738
Büşra Altuntaş <99415208+busraaltuuntas@users.noreply.github.com>
2023-03-10 16:57:04 +0300

    Create SQL3odev.sql

---
+++ SQL3odev.sql
@@ -0,0 +1,5 @@
+1-select country from country where country LIKE 'A%' AND country LIKE '%a'
+2-select country from country Where country LIKE '______' and country LIKE '%n'
+3-select title from film where title ILIKE '%t%t%t%t'
+4-select title from film where title like 'C%' and length>90 and rental_rate=2.99
+
